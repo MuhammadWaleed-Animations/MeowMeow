978486adf03b20fdf7cba5a4d130b74e0e941732
MuhammadWaleed-Animations <WaleedRasool2004@gmail.com>
2025-03-09 22:44:18 +0500

    Lexical Analyzer Beta1.1

--- Compiler Phase1/Source.cpp
+++ Compiler Phase1/Source.cpp
@@ -3,11 +3,7 @@
 #include <string>
 #include <sstream>
 #include <unordered_map>
-
 using namespace std;
-
-
-
 
 class LexicalAnalyzer {
 private:
@@ -93,10 +89,10 @@ private:
         {17, {{'e', 21}, {'E', 21}, {'a', 19}, {'b', 19}, {'c', 19}, {'d', 19}, {'f', 19}, {'g', 19}, {'h', 19}, {'i', 19}, {'j', 19}, {'k', 19}, {'l', 19}, {'m', 19}, {'n', 19}, {'o', 19}, {'p', 19}, {'q', 19}, {'r', 19}, {'s', 19}, {'t', 19}, {'u', 19}, {'v', 19}, {'w', 19}, {'x', 19}, {'y', 19}, {'z', 19}, {'A', 19}, {'B', 19}, {'C', 19}, {'D', 19}, {'F', 19}, {'G', 19}, {'H', 19}, {'I', 19}, {'J', 19}, {'K', 19}, {'L', 19}, {'M', 19}, {'N', 19}, {'O', 19}, {'P', 19}, {'Q', 19}, {'R', 19}, {'S', 19}, {'T', 19}, {'U', 19}, {'V', 19}, {'W', 19}, {'X', 19}, {'Y', 19}, {'Z', 19}, {'_', 19}, {'0', 17}, {'1', 17}, {'2', 17}, {'3', 17}, {'4', 17}, {'5', 17}, {'6', 17}, {'7', 17}, {'8', 17}, {'9', 17}, {'{', 19}, {'}', 19}, {'[', 19}, {']', 19}, {'(', 19}, {')', 19}, {':', 19}, {'=', 19}, {'!', 19}, {'>', 19}, {'<', 19}, {'&', 19}, {'|', 19}, {'+', 19}, {'-', 19}, {'%', 19}, {'*', 19}, {'/', 19}, {'.', 18}, {' ', 19}, {'\t', 19}, {'\r', 19}, {'\n', 19}, {'\0', 19}}},
         {18, {{'e', 21}, {'E', 21}, {'a', 19}, {'b', 19}, {'c', 19}, {'d', 19}, {'f', 19}, {'g', 19}, {'h', 19}, {'i', 19}, {'j', 19}, {'k', 19}, {'l', 19}, {'m', 19}, {'n', 19}, {'o', 19}, {'p', 19}, {'q', 19}, {'r', 19}, {'s', 19}, {'t', 19}, {'u', 19}, {'v', 19}, {'w', 19}, {'x', 19}, {'y', 19}, {'z', 19}, {'A', 19}, {'B', 19}, {'C', 19}, {'D', 19}, {'F', 19}, {'G', 19}, {'H', 19}, {'I', 19}, {'J', 19}, {'K', 19}, {'L', 19}, {'M', 19}, {'N', 19}, {'O', 19}, {'P', 19}, {'Q', 19}, {'R', 19}, {'S', 19}, {'T', 19}, {'U', 19}, {'V', 19}, {'W', 19}, {'X', 19}, {'Y', 19}, {'Z', 19}, {'_', 19}, {'0', 18}, {'1', 18}, {'2', 18}, {'3', 18}, {'4', 18}, {'5', 18}, {'6', 18}, {'7', 18}, {'8', 18}, {'9', 18}, {'{', 19}, {'}', 19}, {'[', 19}, {']', 19}, {'(', 19}, {')', 19}, {':', 19}, {'=', 19}, {'!', 19}, {'>', 19}, {'<', 19}, {'&', 19}, {'|', 19}, {'+', 19}, {'-', 19}, {'%', 19}, {'*', 19}, {'/', 19}, {'.', 19}, {' ', 19}, {'\t', 19}, {'\r', 19}, {'\n', 19}, {'\0', 19}}},
         {20, {{'0', 18}, {'1', 18}, {'2', 18}, {'3', 18}, {'4', 18}, {'5', 18}, {'6', 18}, {'7', 18}, {'8', 18}, {'9', 18}}},
-        {21, {{'0', 22}, {'1', 22}, {'2', 22}, {'3', 22}, {'4', 22}, {'5', 22}, {'6', 22}, {'7', 22}, {'8', 22}, {'9', 22}}},
-        {22, {{'e', 19}, {'E', 19}, {'a', 19}, {'b', 19}, {'c', 19}, {'d', 19}, {'f', 19}, {'g', 19}, {'h', 19}, {'i', 19}, {'j', 19}, {'k', 19}, {'l', 19}, {'m', 19}, {'n', 19}, {'o', 19}, {'p', 19}, {'q', 19}, {'r', 19}, {'s', 19}, {'t', 19}, {'u', 19}, {'v', 19}, {'w', 19}, {'x', 19}, {'y', 19}, {'z', 19}, {'A', 19}, {'B', 19}, {'C', 19}, {'D', 19}, {'F', 19}, {'G', 19}, {'H', 19}, {'I', 19}, {'J', 19}, {'K', 19}, {'L', 19}, {'M', 19}, {'N', 19}, {'O', 19}, {'P', 19}, {'Q', 19}, {'R', 19}, {'S', 19}, {'T', 19}, {'U', 19}, {'V', 19}, {'W', 19}, {'X', 19}, {'Y', 19}, {'Z', 19}, {'_', 19}, {'0', 22}, {'1', 22}, {'2', 22}, {'3', 22}, {'4', 22}, {'5', 22}, {'6', 22}, {'7', 22}, {'8', 22}, {'9', 22}, {'{', 19}, {'}', 19}, {'[', 19}, {']', 19}, {'(', 19}, {')', 19}, {':', 19}, {'=', 19}, {'!', 19}, {'>', 19}, {'<', 19}, {'&', 19}, {'|', 19}, {'+', 23}, {'-', 23}, {'%', 19}, {'*', 19}, {'/', 19}, {'.', 19}}},
+        {21, {{'0', 22}, {'1', 22}, {'2', 22}, {'3', 22}, {'4', 22}, {'5', 22}, {'6', 22}, {'7', 22}, {'8', 22}, {'9', 22}, {'+', 23}, {'-', 23}}},
+        {22, {{'e', 19}, {'E', 19}, {'a', 19}, {'b', 19}, {'c', 19}, {'d', 19}, {'f', 19}, {'g', 19}, {'h', 19}, {'i', 19}, {'j', 19}, {'k', 19}, {'l', 19}, {'m', 19}, {'n', 19}, {'o', 19}, {'p', 19}, {'q', 19}, {'r', 19}, {'s', 19}, {'t', 19}, {'u', 19}, {'v', 19}, {'w', 19}, {'x', 19}, {'y', 19}, {'z', 19}, {'A', 19}, {'B', 19}, {'C', 19}, {'D', 19}, {'F', 19}, {'G', 19}, {'H', 19}, {'I', 19}, {'J', 19}, {'K', 19}, {'L', 19}, {'M', 19}, {'N', 19}, {'O', 19}, {'P', 19}, {'Q', 19}, {'R', 19}, {'S', 19}, {'T', 19}, {'U', 19}, {'V', 19}, {'W', 19}, {'X', 19}, {'Y', 19}, {'Z', 19}, {'_', 19}, {'0', 22}, {'1', 22}, {'2', 22}, {'3', 22}, {'4', 22}, {'5', 22}, {'6', 22}, {'7', 22}, {'8', 22}, {'9', 22}, {'{', 19}, {'}', 19}, {'[', 19}, {']', 19}, {'(', 19}, {')', 19}, {':', 19}, {'=', 19}, {'!', 19}, {'>', 19}, {'<', 19}, {'&', 19}, {'|', 19}, {'+', 19}, {'-', 19}, {'%', 19}, {'*', 19}, {'/', 19}, {'.', 19}, {' ', 19}, {'\t', 19}, {'\r', 19}, {'\n', 19}, {'\0', 19}}},
         {23, {{'0', 22}, {'1', 22}, {'2', 22}, {'3', 22}, {'4', 22}, {'5', 22}, {'6', 22}, {'7', 22}, {'8', 22}, {'9', 22}}},
-        {24, {{'e', 13}, {'E', 13}, {'a', 13}, {'b', 13}, {'c', 13}, {'d', 13}, {'f', 13}, {'g', 13}, {'h', 13}, {'i', 13}, {'j', 13}, {'k', 13}, {'l', 13}, {'m', 13}, {'n', 13}, {'o', 13}, {'p', 13}, {'q', 13}, {'r', 13}, {'s', 13}, {'t', 13}, {'u', 13}, {'v', 13}, {'w', 13}, {'x', 13}, {'y', 13}, {'z', 13}, {'A', 13}, {'B', 13}, {'C', 13}, {'D', 13}, {'F', 13}, {'G', 13}, {'H', 13}, {'I', 13}, {'J', 13}, {'K', 13}, {'L', 13}, {'M', 13}, {'N', 13}, {'O', 13}, {'P', 13}, {'Q', 13}, {'R', 13}, {'S', 13}, {'T', 13}, {'U', 13}, {'V', 13}, {'W', 13}, {'X', 13}, {'Y', 13}, {'Z', 13}, {'_', 13}, {'0', 13}, {'1', 13}, {'2', 13}, {'3', 13}, {'4', 13}, {'5', 13}, {'6', 13}, {'7', 13}, {'8', 13}, {'9', 13}, {'{', 13}, {'}', 13}, {'[', 13}, {']', 13}, {'(', 13}, {')', 13}, {':', 13}, {'=', 13}, {'!', 13}, {'>', 13}, {'<', 13}, {'&', 13}, {'|', 13}, {'+', 13}, {'-', 13}, {'%', 13}, {'.', 13}}},
+        {24, {{'e', -1}, {'E', -1}, {'a', -1}, {'b', -1}, {'c', -1}, {'d', -1}, {'f', -1}, {'g', -1}, {'h', -1}, {'i', -1}, {'j', -1}, {'k', -1}, {'l', -1}, {'m', -1}, {'n', -1}, {'o', -1}, {'p', -1}, {'q', -1}, {'r', -1}, {'s', -1}, {'t', -1}, {'u', -1}, {'v', -1}, {'w', -1}, {'x', -1}, {'y', -1}, {'z', -1}, {'A', -1}, {'B', -1}, {'C', -1}, {'D', -1}, {'F', -1}, {'G', -1}, {'H', -1}, {'I', -1}, {'J', -1}, {'K', -1}, {'L', -1}, {'M', -1}, {'N', -1}, {'O', -1}, {'P', -1}, {'Q', -1}, {'R', -1}, {'S', -1}, {'T', -1}, {'U', -1}, {'V', -1}, {'W', -1}, {'X', -1}, {'Y', -1}, {'Z', -1}, {'_', -1}, {'0', -1}, {'1', -1}, {'2', -1}, {'3', -1}, {'4', -1}, {'5', -1}, {'6', -1}, {'7', -1}, {'8', -1}, {'9', -1}, {'{', -1}, {'}', -1}, {'[', -1}, {']', -1}, {'(', -1}, {')', -1}, {':', -1}, {'=', -1}, {'!', -1}, {'>', -1}, {'<', -1}, {'&', -1}, {'|', -1}, {'+', -1}, {'-', -1}, {'%', -1}, {'*', -1}, {'/', -1}, {'.', -1}, {' ', -1}, {'\t', -1}, {'\r', -1}, {'\n', -1}, {'\0', -1}}},
     };
     std::unordered_map<std::string, int> keywords_table = {
     {"Adadi", 1},
@@ -249,6 +245,11 @@ protected:
             {
                 moveToNextCharacter();
             }
+            if (getNextCharacter() == '\n')
+            {
+                cout << "\n";
+                tokens_file << "\n";
+            }
             moveToNextCharacter();
             ch = getNextCharacter();
         }
@@ -295,12 +296,48 @@ protected:
             cout << "<Error,\"" + complete_lexeme + ">";
             string token = "<Error,\"" + complete_lexeme + ">";
             errors_file << token;
+
+            cout << "\n";
+            tokens_file << "\n";
         }
         else
             cout << "<This wasn't suppose to happen>";
         clearLexeme();
         moveToNextCharacter();
         ch = getNextCharacter();
+    }
+
+    void finalStateHandler(int& state,char& ch)
+    {
+        final_state_information final_state = final_states[state];
+        if (final_state.advance)
+        {
+            moveToNextCharacter();
+            if (!isspace(ch))
+                lexeme.put(ch);
+        }
+        string complete_lexeme = lexeme.str();
+        if (final_state.class_type == "id")
+        {
+            if (keywords_table.find(complete_lexeme) != keywords_table.end())
+            {
+                complete_lexeme = to_string(keywords_table[complete_lexeme]);
+                final_state.class_type = "keyword";
+            }
+            if (symbol_table.find(complete_lexeme) == symbol_table.end())
+            {
+                symbol_table[complete_lexeme] = id_index;
+                complete_lexeme = to_string(id_index++);
+            }
+            else
+            {
+                complete_lexeme = to_string(symbol_table[complete_lexeme]);
+            }
+        }
+        string token = "<" + final_state.class_type + "," + complete_lexeme + ">";
+        cout << token << " ";
+        tokens_file << token;
+        clearLexeme();
     }
     void analyzer()
     {
@@ -310,7 +347,8 @@ protected:
         {
             flag = !input_buffer.empty();
             char ch = getNextCharacter();
-            //cout << state << "-->";
+            //cout << "(" << state << "," << ch << ")" << "-->";
+
             if (ch == '\n')
             {
                 cout << "\n";
@@ -323,33 +361,58 @@ protected:
                     literalMaker(ch);
             }
                 
+            //if (transition_table[24].find(ch) == transition_table[24].end()) //24 is state that have all valid language characters mapped to somewhere
+            //{
+            //    //lexical error
+            //    string token =  ""+ ch;
+            //    token = "<Error,"+token+ ">";
+            //    cout << token;
+            //    errors_file << token;
+            //    ch = ' ';
+            //}
             if(transition_table.find(state) != transition_table.end() && transition_table[state].find(ch) != transition_table[state].end())
             {
+                //cout << "---" << state << "," << ch;
                 state = transition_table[state][ch];
             }
             else
             {
+                //cout << "\nTransition not found\n";
                 if (state == 0)
                 {
                     moveToNextCharacter();
                     if (!isspace(ch))
                         lexeme.put(ch);
                 }
+                
+                
+
                 string complete_lexeme = lexeme.str();
+                //cout << "\n Complete Lexeme " << complete_lexeme << "\n";
                 if (keywords_table.find(complete_lexeme) != keywords_table.end())
                 {
                     complete_lexeme = to_string(keywords_table[complete_lexeme]);
-                    tokens_file << ("<keyword,"+complete_lexeme+">");
+                    tokens_file << ("<keyword," + complete_lexeme + ">");
                     cout << "<Keyword," + complete_lexeme + ">";
                 }
                 else if ((complete_lexeme == "input" && getNextCharacter() == '-' && getNextNextCharacter() == '>') || (complete_lexeme == "output" && getNextCharacter() == '<' && getNextNextCharacter() == '-'))
                 {
-                    complete_lexeme = complete_lexeme + getNextCharacter()+ "" + getNextNextCharacter();
+                    complete_lexeme = complete_lexeme + getNextCharacter() + "" + getNextNextCharacter();
                     moveToNextCharacter();
                     moveToNextCharacter();
                     cout << "<Keyword," + complete_lexeme + ">";
                     complete_lexeme = to_string(keywords_table[complete_lexeme]);
-                    tokens_file << ("<keyword," + complete_lexeme + ">");                                        
+                    tokens_file << ("<keyword," + complete_lexeme + ">");
+                }
+                else if (transition_table[state].find(' ')!= transition_table[state].end() && final_states.find(transition_table[state][' ']) != final_states.end())
+                {
+                    finalStateHandler(transition_table[state][' '], ch);
+                    moveToNextCharacter();
+                    lexeme.put(ch);
+                    string lexical_error_token = "<Error," + lexeme.str()+">";
+                    cout << lexical_error_token;
+                    errors_file << lexical_error_token;
+                    state = 0;
                 }
                 else
                 {
@@ -360,41 +423,14 @@ protected:
 
                 clearLexeme();
                 state = 0; //error
+                
                 continue;
             }
             //if(! (final_states.find(state) != final_states.end() && !final_states[state].advance))
             //if(final_states.find(state) == final_states.end() || final_states[state].advance)
             if (final_states.find(state) != final_states.end())
             {
-                final_state_information final_state = final_states[state];
-                if (final_state.advance)
-                {
-                    moveToNextCharacter();
-                    if (!isspace(ch))
-                        lexeme.put(ch);
-                }             
-                string complete_lexeme = lexeme.str();
-                if (final_state.class_type == "id")
-                {
-                    if (keywords_table.find(complete_lexeme) != keywords_table.end())
-                    {
-                        complete_lexeme = to_string(keywords_table[complete_lexeme]);
-                        final_state.class_type = "keyword";
-                    }
-                    if (symbol_table.find(complete_lexeme) == symbol_table.end())
-                    {
-                        symbol_table[complete_lexeme] = id_index;
-                        complete_lexeme = to_string(id_index++);
-                    }
-                    else
-                    {
-                        complete_lexeme = to_string(symbol_table[complete_lexeme]);
-                    }
-                }
-                string token = "<" + final_state.class_type + "," + complete_lexeme + ">";
-                cout << token << " ";
-                tokens_file << token;
-                clearLexeme();
+                finalStateHandler(state, ch);
                 state = 0;               
             }  
             else
@@ -404,8 +440,10 @@ protected:
                     lexeme.put(ch);
             }
         }
-        cout << state;
+        //cout << state;
     }
+
+
     void writeTables()
     {
         for (auto entry : symbol_table)
@@ -475,5 +513,6 @@ int main() {
     LexicalAnalyzer* sad = new LexicalAnalyzer("input.txt");
     delete sad;
     
+    system("pause");
     return 0;
 }
